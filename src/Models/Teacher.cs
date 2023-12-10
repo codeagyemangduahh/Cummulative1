070cb2b4a550a897ce34ab13ebb1f5bb05fdc3bf
Chichi <test@C02V40XBGFWK-10.15>
2023-12-10 00:08:41 -0500

    MVP

---
+++ src/Models/Teacher.cs
@@ -0,0 +1,12 @@
+namespace SchoolApp.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string? EmployeeNumber { get; set; }
+    public DateTime HireDate { get; set; }
+    public decimal Salary { get; set; }
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+}
